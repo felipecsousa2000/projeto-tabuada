9df788b32f2f41e531500a88f7cf2d76304ef74a
felipe <felipecsousa2000@gmail.com>
2024-10-01 14:48:31 -0300

    projeto-tabuada

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// Selecione os elementos
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+const multiplicationTitle = document.querySelector("#multiplication-title span");
+const multiplicationTable = document.querySelector("#multiplication-operations");
+
+// Funções
+const createTable = (number, multiplicatorNumber) => {
+    multiplicationTable.innerHTML = "";//para limpar o campo da operação
+
+    for(let i = 1; i <= multiplicatorNumber; i++) {
+        const result = number * i;
+        
+        const template = `<div class="row">
+            <div class="operation">${number} x ${i} =</div>
+            <div class="result">${result}</div>
+        </div>`; 
+
+        const parser = new DOMParser();
+        const htmlTemplate = parser.parseFromString(template, "text/html");
+        const row = htmlTemplate.querySelector(".row");
+
+        multiplicationTable.appendChild(row);
+    }
+
+    multiplicationTitle.innerHTML = number;
+};
+
+// Eventos
+multiplicationForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const multiplicationNumber = +numberInput.value;
+    const multiplicatorNumber = +multiplicationInput.value;
+
+    if (!multiplicationNumber || !multiplicatorNumber) return;// SE ALGUM DOS NÚMEROS FALTAR NÃO VAI FUNCIONAR
+
+    console.log(multiplicationNumber, multiplicatorNumber);
+    createTable(multiplicationNumber, multiplicatorNumber);
+});
